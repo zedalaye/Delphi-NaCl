ef01a140f30d6f6a1dc687b9c8eba6fcbfdc1017
Pierre Yager <pierre.yager@crisalid.com>
2023-11-11 23:28:11 +0100

    Added Sodium.Box with methods to compute authenticated encryption using public key algorithms

---
+++ examples/test_box.dpr
@@ -0,0 +1,124 @@
+program test_box;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  libsodium in '..\lib\libsodium.pas',
+  Sodium.Utils in '..\lib\Sodium.Utils.pas',
+  Sodium.Box in '..\lib\Sodium.Box.pas';
+
+procedure test_api(Nonce: TCryptoBoxNonce;
+  AlicePublicKey: TCryptoBoxPublicKey; AliceSecretKey: TCryptoBoxSecretKey;
+  BobPublicKey: TCryptoBoxPublicKey; BobSecretKey: TCryptoBoxSecretKey);
+var
+  M: TBytes;
+  ciphertext, cleartext: TBytes;
+begin
+  M := TEncoding.UTF8.GetBytes('test');
+  SetLength(ciphertext, _CRYPTO_BOX_MACBYTES + Length(M));
+
+  if crypto_box_easy(@ciphertext[0], @M[0], Length(M), @Nonce[0], @BobPublicKey[0], @AliceSecretKey[0]) <> 0 then
+  begin
+    WriteLn('crypto_box_easy() => FAILED');
+    Exit;
+  end;
+
+  SetLength(cleartext, Length(M));
+  if crypto_box_open_easy(@cleartext[0], @ciphertext[0], Length(ciphertext), @Nonce[0], @AlicePublicKey[0], @BobSecretKey[0]) <> 0 then
+    WriteLn('crypto_box_open_easy() => FAILED')
+  else
+    WriteLn('crypto_box() => OK');
+end;
+
+procedure test(Nonce: TCryptoBoxNonce;
+  AlicePublicKey: TCryptoBoxPublicKey; AliceSecretKey: TCryptoBoxSecretKey;
+  BobPublicKey: TCryptoBoxPublicKey; BobSecretKey: TCryptoBoxSecretKey);
+var
+  CipherBuf, ClearBuf: TBytes;
+begin
+  if TCryptoBox.Easy(CipherBuf, TEncoding.UTF8.GetBytes('test'), Nonce, BobPublicKey, AliceSecretKey) then
+    if TCryptoBox.OpenEasy(ClearBuf, CipherBuf, Nonce, AlicePublicKey, BobSecretKey) then
+      if TEncoding.UTF8.GetString(ClearBuf) = 'test' then
+        WriteLn('SUCCESS')
+      else
+        WriteLn('FAILED (CipherBuf <> ''test'')')
+    else
+      WriteLn('FAILED (TCryptoBox.OpenEasy())')
+  else
+    WriteLn('FAILED (TCryptoBox.Easy())');
+end;
+
+procedure test_detached(Nonce: TCryptoBoxNonce;
+  AlicePublicKey: TCryptoBoxPublicKey; AliceSecretKey: TCryptoBoxSecretKey;
+  BobPublicKey: TCryptoBoxPublicKey; BobSecretKey: TCryptoBoxSecretKey);
+var
+  CipherBuf, ClearBuf: TBytes;
+  AuthTag: TCryptoBoxMac;
+begin
+  if TCryptoBox.Detached(CipherBuf, AuthTag, TEncoding.UTF8.GetBytes('test'), Nonce, BobPublicKey, AliceSecretKey) then
+    if TCryptoBox.OpenDetached(ClearBuf, CipherBuf, AuthTag, Nonce, AlicePublicKey, BobSecretKey) then
+      if TEncoding.UTF8.GetString(ClearBuf) = 'test' then
+        WriteLn('SUCCESS')
+      else
+        WriteLn('FAILED (CipherBuf <> ''test'')')
+    else
+      WriteLn('FAILED (TCryptoBox.OpenDetached())')
+  else
+    WriteLn('FAILED (TCryptoBox.Detached())');
+end;
+
+procedure test_seal(RecipientPublicKey: TCryptoBoxPublicKey; RecipientSecretKey: TCryptoBoxSecretKey);
+var
+  CipherBuf, ClearBuf: TBytes;
+begin
+  if TCryptoBox.Seal(CipherBuf, TEncoding.UTF8.GetBytes('test'), RecipientPublicKey) then
+    if TCryptoBox.OpenSeal(ClearBuf, CipherBuf, RecipientPublicKey, RecipientSecretKey) then
+      if TEncoding.UTF8.GetString(ClearBuf) = 'test' then
+        WriteLn('SUCCESS')
+      else
+        WriteLn('FAILED (CipherBuf <> ''test'')')
+    else
+      WriteLn('FAILED (TCryptoBox.OpenSeal())')
+  else
+    WriteLn('FAILED (TCryptoBox.Seal())');
+end;
+
+var
+  AlicePublicKey: TCryptoBoxPublicKey;
+  AliceSecretKey: TCryptoBoxSecretKey;
+
+  BobPublicKey: TCryptoBoxPublicKey;
+  BobSecretKey: TCryptoBoxSecretKey;
+
+  Nonce: TCryptoBoxNonce;
+
+begin
+  try
+    if not TCryptoBox.Keypair(AlicePublicKey, AliceSecretKey) then
+    begin
+      WriteLn('CryptoBox.Keypair(ALICE) FAILED');
+      Exit;
+    end;
+
+    if not TCryptoBox.Keypair(BobPublicKey, BobSecretKey) then
+    begin
+      WriteLn('CryptoBox.Keypair(BOB) FAILED');
+      Exit;
+    end;
+
+    Nonce := TCryptoBox.Nonce;
+
+    Write('API...'); test_api(Nonce, AlicePublicKey, AliceSecretKey, BobPublicKey, BobSecretKey);
+    Write('Wrapper...'); test(Nonce, AlicePublicKey, AliceSecretKey, BobPublicKey, BobSecretKey);
+    Write('Wrapper (DETACHED)...'); test_detached(Nonce, AlicePublicKey, AliceSecretKey, BobPublicKey, BobSecretKey);
+    Write('Wrapper (SEAL)...'); test_seal(BobPublicKey, BobSecretKey);
+
+    ReadLn;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
---
+++ lib/Sodium.Box.pas
@@ -0,0 +1,125 @@
+unit Sodium.Box;
+
+interface
+
+uses
+  System.SysUtils,
+  libsodium, Sodium.Utils;
+
+type
+  TCryptoBox = record
+    class function Primitive: string; static;
+
+    class function Keypair(var PublicKey: TCryptoBoxPublicKey; var SecretKey: TCryptoBoxSecretKey): Boolean; static;
+
+    class function Nonce: TCryptoBoxNonce; static;
+
+    class function Easy(
+      var CipheredBuf: TBytes;
+      ClearBuf: TBytes;
+      Nonce: TCryptoBoxNonce;
+      PublicKey: TCryptoBoxPublicKey; SecretKey: TCryptoBoxSecretKey): Boolean; static;
+
+    class function OpenEasy(
+      var ClearBuf: TBytes;
+      CipheredBuf: TBytes;
+      Nonce: TCryptoBoxNonce;
+      PublicKey: TCryptoBoxPublicKey; SecretKey: TCryptoBoxSecretKey): Boolean; static;
+
+    class function Detached(
+      var CipheredBuf: TBytes; var AuthTag: TCryptoBoxMac;
+      ClearBuf: TBytes;
+      Nonce: TCryptoBoxNonce;
+      PublicKey: TCryptoBoxPublicKey; SecretKey: TCryptoBoxSecretKey): Boolean; static;
+
+    class function OpenDetached(
+      var ClearBuf: TBytes;
+      CipheredBuf: TBytes; AuthTag: TCryptoBoxMac;
+      Nonce: TCryptoBoxNonce;
+      PublicKey: TCryptoBoxPublicKey; SecretKey: TCryptoBoxSecretKey): Boolean; static;
+
+    class function Seal(
+      var CipheredBuf: TBytes;
+      ClearBuf: TBytes;
+      PublicKey: TCryptoBoxPublicKey): Boolean; static;
+
+    class function OpenSeal(
+      var ClearBuf: TBytes;
+      CipheredBuf: TBytes;
+      PublicKey: TCryptoBoxPublicKey; SecretKey: TCryptoBoxSecretKey): Boolean; static;
+  end;
+
+implementation
+
+{ TCryptoBox }
+
+class function TCryptoBox.Primitive: string;
+begin
+  Result := string(crypto_box_primitive);
+end;
+
+class function TCryptoBox.Keypair(var PublicKey: TCryptoBoxPublicKey;
+  var SecretKey: TCryptoBoxSecretKey): Boolean;
+begin
+  Result := crypto_box_keypair(@PublicKey[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoBox.Nonce: TCryptoBoxNonce;
+begin
+  randombytes_buf(@Result[0], SizeOf(Result));
+end;
+
+class function TCryptoBox.Easy(var CipheredBuf: TBytes; ClearBuf: TBytes;
+  Nonce: TCryptoBoxNonce; PublicKey: TCryptoBoxPublicKey;
+  SecretKey: TCryptoBoxSecretKey): Boolean;
+begin
+  SetLength(CipheredBuf, _CRYPTO_BOX_MACBYTES + Length(ClearBuf));
+  Result := crypto_box_easy(@CipheredBuf[0], @ClearBuf[0], Length(ClearBuf),
+              @Nonce[0], @PublicKey[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoBox.OpenEasy(var ClearBuf: TBytes; CipheredBuf: TBytes;
+  Nonce: TCryptoBoxNonce; PublicKey: TCryptoBoxPublicKey;
+  SecretKey: TCryptoBoxSecretKey): Boolean;
+begin
+  SetLength(ClearBuf, Length(CipheredBuf) - _CRYPTO_BOX_MACBYTES);
+  Result := crypto_box_open_easy(@ClearBuf[0], @CipheredBuf[0], Length(CipheredBuf),
+              @Nonce[0], @PublicKey[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoBox.Detached(var CipheredBuf: TBytes;
+  var AuthTag: TCryptoBoxMac; ClearBuf: TBytes; Nonce: TCryptoBoxNonce;
+  PublicKey: TCryptoBoxPublicKey; SecretKey: TCryptoBoxSecretKey): Boolean;
+begin
+  SetLength(CipheredBuf, Length(ClearBuf));
+  Result := crypto_box_detached(@CipheredBuf[0], @AuthTag[0],
+              @ClearBuf[0], Length(ClearBuf),
+              @Nonce[0], @PublicKey[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoBox.OpenDetached(var ClearBuf: TBytes;
+  CipheredBuf: TBytes; AuthTag: TCryptoBoxMac; Nonce: TCryptoBoxNonce;
+  PublicKey: TCryptoBoxPublicKey; SecretKey: TCryptoBoxSecretKey): Boolean;
+begin
+  SetLength(ClearBuf, Length(CipheredBuf));
+  Result := crypto_box_open_detached(@ClearBuf[0], @CipheredBuf[0], @AuthTag[0], Length(CipheredBuf),
+              @Nonce[0], @PublicKey[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoBox.Seal(var CipheredBuf: TBytes; ClearBuf: TBytes;
+  PublicKey: TCryptoBoxPublicKey): Boolean;
+begin
+  SetLength(CipheredBuf, Length(ClearBuf) + _CRYPTO_BOX_SEALBYTES);
+  Result := crypto_box_seal(@CipheredBuf[0], @ClearBuf[0], Length(ClearBuf),
+              @PublicKey[0]) = 0;
+end;
+
+class function TCryptoBox.OpenSeal(var ClearBuf: TBytes; CipheredBuf: TBytes;
+  PublicKey: TCryptoBoxPublicKey; SecretKey: TCryptoBoxSecretKey): Boolean;
+begin
+  SetLength(ClearBuf, Length(CipheredBuf) - _CRYPTO_BOX_SEALBYTES);
+  Result := crypto_box_seal_open(@ClearBuf[0], @CipheredBuf[0], Length(CipheredBuf),
+              @PublicKey[0], @SecretKey[0]) = 0;
+end;
+
+end.
