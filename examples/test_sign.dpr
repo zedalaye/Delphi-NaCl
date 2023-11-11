3676d38bfe0be38d5a7b59c7542c7f9b30483806
Pierre Yager <pierre.yager@crisalid.com>
2023-11-11 23:30:35 +0100

    Added Sodium.sign with methods to compute signatures using public key cryptography

---
+++ examples/test_sign.dpr
@@ -0,0 +1,169 @@
+program test_sign;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  libsodium in '..\lib\libsodium.pas',
+  Sodium.Utils in '..\lib\Sodium.Utils.pas',
+  Sodium.Sign in '..\lib\Sodium.Sign.pas';
+
+procedure test_api(PublicKey: TCryptoSignPublicKey; SecretKey: TCryptoSignSecretKey);
+var
+  sig: TCryptoSignature;
+begin
+  WriteLn(crypto_sign_primitive);
+
+  var M := TEncoding.UTF8.GetBytes('test');
+
+  var signed_message: TBytes; var signed_message_len: UInt64;
+  SetLength(signed_message, Length(M) + _CRYPTO_SIGN_BYTES);
+
+  crypto_sign(@signed_message[0], signed_message_len, @M[0], Length(M), @SecretKey[0]);
+
+  WriteLn('crypto_sign() => ', THexEncode.FromBytes(@signed_message[0], signed_message_len));
+
+  var unsigned_message: TBytes; var unsigned_message_len: UInt64;
+  SetLength(unsigned_message, Length(M));
+
+  if crypto_sign_open(@unsigned_message[0], unsigned_message_len, @signed_message[0], signed_message_len, @PublicKey[0]) <> 0 then
+    WriteLn('crypto_sign_open() => FAILED')
+  else
+    WriteLn('crypto_sign_open() => SUCCESS');
+
+  var sig_len: UInt64;
+  crypto_sign_detached(@sig[0], sig_len, @M[0], Length(M), @SecretKey[0]);
+
+  WriteLn('crypto_sign_detached() => ', THexEncode.FromBytes(@sig[0], sig_len));
+
+  if crypto_sign_verify_detached(@sig[0], @M[0], Length(M), @PublicKey[0]) <> 0 then
+    WriteLn('crypto_sign_verify_detached() => FAILED')
+  else
+    WriteLn('crypto_sign_verify_detached() => SUCCESS');
+end;
+
+procedure test(PublicKey: TCryptoSignPublicKey; SecretKey: TCryptoSignSecretKey);
+var
+  SignedBuf: TBytes;
+  UnsignedBuf: TBytes;
+  Signature: TCryptoSignature;
+begin
+  WriteLn(TCryptoSign.Primitive);
+
+  if TCryptoSign.Sign(SignedBuf, TEncoding.UTF8.GetBytes('test'), SecretKey) then
+    if TCryptoSign.OpenSign(UnsignedBuf, SignedBuf, PublicKey) then
+      if TEncoding.UTF8.GetString(UnsignedBuf) = 'test' then
+      begin
+        WriteLn('Embedded Signature : SUCCESS');
+        WriteLn('SignedBuf=', THexEncode.FromBytes(SignedBuf));
+      end
+      else
+        WriteLn('FAILED (Buffers do not match)')
+    else
+      WriteLn('FAILED (TCryptoSign.OpenSign())')
+  else
+    WriteLn('FAILED (TCryptoSign.Sign())');
+
+  if TCryptoSign.Detached(Signature, TEncoding.UTF8.GetBytes('test'), SecretKey) then
+    if TCryptoSign.VerifyDetached(Signature, TEncoding.UTF8.GetBytes('test'), PublicKey) then
+    begin
+      WriteLn('Detached Signature : SUCCESS');
+      WriteLn('Signature=', THexEncode.FromBytes(Signature, SizeOf(Signature)));
+    end
+    else
+      WriteLn('FAILED (TCryptoSign.VerifyDetached())')
+  else
+    WriteLn('FAILED (TCryptoSign.Detached())');
+end;
+
+procedure test_seed(PublicKey: TCryptoSignPublicKey; SecretKey: TCryptoSignSecretKey);
+var
+  Seed: TCryptoSignSeed;
+  TestPK: TCryptoSignPublicKey;
+begin
+  if TCryptoSign.SecretKeyToSeed(Seed, SecretKey) then
+  begin
+    WriteLn('SecretKeyToSeed : SUCCESS');
+    WriteLn('Seed=', THexEncode.FromBytes(Seed, SizeOf(Seed)));
+  end
+  else
+    WriteLn('FAILED (TCryptoSign.SecretKeyToSeed())');
+
+  if TCryptoSign.SecretKeyToPublicKey(TestPK, SecretKey) then
+  begin
+    if TBytes.Same<TCryptoSignPublicKey>(TestPK, PublicKey) then
+    begin
+      WriteLn('SecretKeyToPublicKey : SUCCESS');
+      WriteLn('PublicKey=', THexEncode.FromBytes(TestPK, SizeOf(TestPK)));
+    end
+    else
+      WriteLn('FAILED (Public Keys do not match)');
+  end
+  else
+    WriteLn('FAILED (TCryptoSign.SecretKetToPublicKey())');
+end;
+
+procedure test_multipart(PublicKey: TCryptoSignPublicKey; SecretKey: TCryptoSignSecretKey);
+const
+  M: array[0..2] of string = ('Random data ', 'to be signed ', 'split on multiple lines');
+var
+  Signature: TCryptoSignature;
+  I: Integer;
+begin
+  I := 0;
+  if TCryptoSign.SignMultipart(Signature,
+       procedure(var Buffer: TBytes; var Done: Boolean)
+       begin
+         Buffer := TEncoding.UTF8.GetBytes(M[I]);
+         Inc(I);
+         Done := I > High(M);
+       end,
+       SecretKey
+    )
+  then
+  begin
+    WriteLn('SignMultipart : SUCESS');
+    WriteLn('Signature=', THexEncode.FromBytes(Signature, SizeOf(Signature)));
+  end;
+
+  I := 0;
+  if TCryptoSign.VerifyMutlipart(Signature,
+       procedure(var Buffer: TBytes; var Done: Boolean)
+       begin
+         Buffer := TEncoding.UTF8.GetBytes(M[I]);
+         Inc(I);
+         Done := I > High(M);
+       end,
+       PublicKey
+     )
+  then
+  begin
+    WriteLn('VerifyMultipart : SUCESS');
+  end;
+end;
+
+var
+  pk: TCryptoSignPublicKey;
+  sk: TCryptoSignSecretKey;
+
+begin
+  try
+    if not TCryptoSign.Keypair(pk, sk) then
+    begin
+      WriteLn('TCryptoSign.Keypair() => FAILED');
+      Exit;
+    end;
+
+    Write('API...');     test_api(pk, sk);
+    Write('Wrapper...'); test(pk, sk);
+    Write('Wrapper...'); test_seed(pk, sk);
+    Write('Wrapper...'); test_multipart(pk, sk);
+
+    ReadLn;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
---
+++ lib/Sodium.Sign.pas
@@ -0,0 +1,164 @@
+unit Sodium.Sign;
+
+interface
+
+uses
+  System.SysUtils,
+  libsodium, Sodium.Utils;
+
+type
+  TCryptoSign = record
+    class function Primitive: string; static;
+
+    class function Keypair(var PublicKey: TCryptoSignPublicKey; var SecretKey: TCryptoSignSecretKey): Boolean; static;
+    class function SeedKeypair(var PublicKey: TCryptoSignPublicKey; var SecretKey: TCryptoSignSecretKey; Seed: TCryptoSignSeed): Boolean; static;
+
+    class function SecretKeyToSeed(var Seed: TCryptoSignSeed; SecretKey: TCryptoSignSecretKey): Boolean; static;
+    class function SecretKeyToPublicKey(var PublicKey: TCryptoSignPublicKey; SecretKey: TCryptoSignSecretKey): Boolean; static;
+
+    class function PublicKeyToCurve25519(var Curve25519PublicKey: TCryptoScalarMultCurve25519Key; PublicKey: TCryptoSignPublicKey): Boolean; static;
+    class function SecretKeyToCurve25519(var Curve25519SecretKey: TCryptoScalarMultCurve25519Key; SecretKey: TCryptoSignSecretKey): Boolean; static;
+
+    class function Sign(var SignedBuf: TBytes; const ClearBuf: TBytes; const SecretKey: TCryptoSignSecretKey): Boolean; static;
+    class function OpenSign(var UnsignedBuf: TBytes; const SignedBuf: TBytes; const PublicKey: TCryptoSignPublicKey): Boolean; static;
+
+    class function Detached(var Signature: TCryptoSignature; const ClearBuf: TBytes; const SecretKey: TCryptoSignSecretKey): Boolean; static;
+    class function VerifyDetached(Signature: TCryptoSignature; const ClearBuf: TBytes; const PublicKey: TCryptoSignPublicKey): Boolean; static;
+
+    class function SignMultipart(var Signature: TCryptoSignature; const InProc: TCryptoDataProc; const SecretKey: TCryptoSignSecretKey): Boolean; static;
+    class function VerifyMutlipart(Signature: TCryptoSignature; const InProc: TCryptoDataProc; const PublicKey: TCryptoSignPublicKey): Boolean; static;
+  end;
+
+implementation
+
+{ TCryptoSign }
+
+class function TCryptoSign.Primitive: string;
+begin
+  Result := string(crypto_sign_primitive);
+end;
+
+class function TCryptoSign.Keypair(var PublicKey: TCryptoSignPublicKey;
+  var SecretKey: TCryptoSignSecretKey): Boolean;
+begin
+  Result := crypto_sign_keypair(@PublicKey[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoSign.SeedKeypair(var PublicKey: TCryptoSignPublicKey;
+  var SecretKey: TCryptoSignSecretKey; Seed: TCryptoSignSeed): Boolean;
+begin
+  Result := crypto_sign_seed_keypair(@PublicKey[0], @SecretKey[0], @Seed[0]) = 0;
+end;
+
+class function TCryptoSign.SecretKeyToSeed(var Seed: TCryptoSignSeed;
+  SecretKey: TCryptoSignSecretKey): Boolean;
+begin
+  Result := crypto_sign_ed25519_sk_to_seed(@Seed[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoSign.SecretKeyToPublicKey(
+  var PublicKey: TCryptoSignPublicKey;
+  SecretKey: TCryptoSignSecretKey): Boolean;
+begin
+  Result := crypto_sign_ed25519_sk_to_pk(@PublicKey[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoSign.PublicKeyToCurve25519(
+  var Curve25519PublicKey: TCryptoScalarMultCurve25519Key;
+  PublicKey: TCryptoSignPublicKey): Boolean;
+begin
+  Result := crypto_sign_ed25519_pk_to_curve25519(@Curve25519PublicKey[0], @PublicKey[0]) = 0;
+end;
+
+class function TCryptoSign.SecretKeyToCurve25519(
+  var Curve25519SecretKey: TCryptoScalarMultCurve25519Key;
+  SecretKey: TCryptoSignSecretKey): Boolean;
+begin
+  Result := crypto_sign_ed25519_sk_to_curve25519(@Curve25519SecretKey[0], @SecretKey[0]) = 0;
+end;
+
+class function TCryptoSign.Sign(var SignedBuf: TBytes; const ClearBuf: TBytes;
+  const SecretKey: TCryptoSignSecretKey): Boolean;
+var
+  SignedBufLen: UInt64;
+begin
+  SetLength(SignedBuf, Length(ClearBuf) + _CRYPTO_SIGN_BYTES);
+  Result := crypto_sign(@SignedBuf[0], SignedBufLen, @ClearBuf[0], Length(ClearBuf), @SecretKey[0]) = 0;
+  SetLength(SignedBuf, SignedBufLen);
+end;
+
+class function TCryptoSign.OpenSign(var UnsignedBuf: TBytes;
+  const SignedBuf: TBytes; const PublicKey: TCryptoSignPublicKey): Boolean;
+var
+  UnsignedBufLen: UInt64;
+begin
+  SetLength(UnsignedBuf, Length(SignedBuf) - _CRYPTO_SIGN_BYTES);
+  Result := crypto_sign_open(@UnsignedBuf[0], UnsignedBufLen,
+              @SignedBuf[0], Length(SignedBuf), @PublicKey[0]) = 0;
+  SetLength(UnsignedBuf, UnsignedBufLen);
+end;
+
+class function TCryptoSign.Detached(var Signature: TCryptoSignature;
+  const ClearBuf: TBytes; const SecretKey: TCryptoSignSecretKey): Boolean;
+var
+  _SignatureLen: UInt64; // safe to ignore, Signature is always _CRYPTO_SIGN_BYTES long
+begin
+  Result := crypto_sign_detached(@Signature[0], _SignatureLen,
+              @ClearBuf[0], Length(ClearBuf), @SecretKey[0]) = 0;
+end;
+
+class function TCryptoSign.VerifyDetached(Signature: TCryptoSignature;
+  const ClearBuf: TBytes; const PublicKey: TCryptoSignPublicKey): Boolean;
+begin
+  Result := crypto_sign_verify_detached(@Signature[0],
+              @ClearBuf[0], Length(ClearBuf), @PublicKey[0]) = 0;
+end;
+
+class function TCryptoSign.SignMultipart(var Signature: TCryptoSignature;
+  const InProc: TCryptoDataProc;
+  const SecretKey: TCryptoSignSecretKey): Boolean;
+var
+  State: TCryptoSignState;
+  Done: Boolean;
+  Buffer: TBytes;
+  SignatureLen: UInt64;
+begin
+  if crypto_sign_init(State) <> 0 then
+    Exit(False);
+
+  Done := False;
+  while not Done do
+  begin
+    InProc(Buffer, Done);
+    if crypto_sign_update(State, @Buffer[0], Length(Buffer)) <> 0 then
+      Exit(False);
+  end;
+
+  Result := crypto_sign_final_create(State,
+              @Signature[0], SignatureLen, @SecretKey[0]) = 0;
+end;
+
+class function TCryptoSign.VerifyMutlipart(Signature: TCryptoSignature;
+  const InProc: TCryptoDataProc;
+  const PublicKey: TCryptoSignPublicKey): Boolean;
+var
+  State: TCryptoSignState;
+  Done: Boolean;
+  Buffer: TBytes;
+begin
+  if crypto_sign_init(State) <> 0 then
+    Exit(False);
+
+  Done := False;
+  while not Done do
+  begin
+    InProc(Buffer, Done);
+    if crypto_sign_update(State, @Buffer[0], Length(Buffer)) <> 0 then
+      Exit(False);
+  end;
+
+  Result := crypto_sign_final_verify(State,
+              @Signature[0], @PublicKey[0]) = 0;
+end;
+
+end.
